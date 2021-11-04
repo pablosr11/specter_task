e2eb0d692be5413391eb2486d60aad65ea6db3ea
ps <pablosr11@gmail.com>
2021-11-04 23:49:35 +0000

    Add pipeline fn. Request data

--- main.py
+++ main.py
@@ -5,8 +5,37 @@ from requests.models import Response
 import sqlite3
 from discord import Webhook, RequestsWebhookAdapter
 
+# To check consistency.
+API_KEYS = {'maxSupply', 'isAudited', 'totalSupply',
+            'dateAdded', 'auditInfoList', 'slug', 'cmcRank',
+            'name', 'isActive', 'symbol', 'lastUpdated',
+            'quotes', 'marketPairCount', 'circulatingSupply',
+            'id', 'platform', 'tags'}
+
 URL = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?limit=100"
 con: Connection = sqlite3.connect('example.db')
+
+
+def parse_response(res: Response) -> tuple[dict, list[dict]]:
+    rjson = res.json()
+    status: dict = rjson["status"]
+    currencies: list[dict] = rjson["data"]['cryptoCurrencyList']
+    # count: int = data['totalCount']
+    return status, currencies
+
+def pipeline():
+
+    # Get data
+    r: Response = requests.get(URL)
+    r.raise_for_status()
+
+    # Extract data
+    try:
+        status, currencies = parse_response(r)
+    except KeyError as key_err:
+        raise key_err
+    except json.JSONDecodeError as json_err:
+        raise json_err
 
 def setup(conn: Connection):
     conn.execute("""
@@ -37,3 +66,4 @@ def setup(conn: Connection):
 
 if __name__ == "__main__":
     setup(conn=con)
+    pipeline()
