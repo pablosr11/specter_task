037ff7c69a1fc7f7cce92981ce7e54d3b0ec658f
ps <pablosr11@gmail.com>
2021-11-04 23:51:50 +0000

    Add storage fn and discord webhook

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ API_KEYS = {'maxSupply', 'isAudited', 'totalSupply',
             'quotes', 'marketPairCount', 'circulatingSupply',
             'id', 'platform', 'tags'}
 
+
+DISCORD_GENERAL = ""
+webhook: Webhook = Webhook.from_url(
+    DISCORD_GENERAL, adapter=RequestsWebhookAdapter())
+
 URL = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?limit=100"
 con: Connection = sqlite3.connect('example.db')
 
@@ -22,6 +27,7 @@ def parse_response(res: Response) -> tuple[dict, list[dict]]:
     currencies: list[dict] = rjson["data"]['cryptoCurrencyList']
     # count: int = data['totalCount']
     return status, currencies
+
 
 def aggregate_data(currencies: list[dict]) -> tuple[list, list]:
     coins: list[tuple] = []
@@ -36,6 +42,26 @@ def aggregate_data(currencies: list[dict]) -> tuple[list, list]:
                           quote["percentChange24h"], quote["percentChange7d"],
                           quote["ytdPriceChangePercentage"], quote["lastUpdated"]))
     return coins, quotes
+
+
+def store(conn: Connection, coins: list, quotes: list):
+    # COINS
+    conn.executemany(
+        """
+        INSERT OR IGNORE INTO coins (coin_id, name, slug, symbol, date_added) 
+        VALUES (?,?,?,?,?)
+        """, coins)
+    conn.commit()
+
+    # QUOTES
+    conn.executemany(
+        """
+        INSERT INTO quotes (coin_id, coin, currency, price, volume, mc, chg1h, chg24h, chg7d, ytdchg, lastupdated) 
+        VALUES (?,?,?,?,?,?,?,?,?,?,?)
+        """, quotes)
+    conn.commit()
+
+
 def pipeline():
 
     # Get data
@@ -46,8 +72,10 @@ def pipeline():
     try:
         status, currencies = parse_response(r)
     except KeyError as key_err:
+        webhook.send("Key Error when parsing API. Check new api keys.")
         raise key_err
     except json.JSONDecodeError as json_err:
+        webhook.send("JSON Decoding error when parsing API")
         raise json_err
 
     # Validate - right types, right size, expected data...
@@ -55,6 +83,11 @@ def pipeline():
 
     # Aggregate
     coins, quotes = aggregate_data(currencies=currencies)
+
+    # Storage
+    store(conn=con, coins=coins, quotes=quotes)
+
+
 def setup(conn: Connection):
     conn.execute("""
         CREATE TABLE IF NOT EXISTS quotes (
